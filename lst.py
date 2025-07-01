aae9069bddd9761010901c3a97eb2b41a50802d0
VinodG546 <gudlavinod546@gmail.com>
2025-07-01 14:48:12 +0530

    two python files

---
+++ lst.py
@@ -0,0 +1,6 @@
+lst = [(None, None), (1, 2), (None, 3), (4, 5), (None, None)]
+filterd=[]
+for tup in lst:
+    if any(elem is not None for elem in tup):
+        filterd.append(tup)
+print(filterd)
